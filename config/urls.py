687711767d1b699b5ffdf63fa04d29886b2d09bc
jerry <cat.ion2@hotmail.com>
2026-07-15 15:27:31 +0800

    feat: update pages templates

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path("",include("pages.urls",namespace='pages')),
     path('admin/', admin.site.urls),
-    path("",include("pages.urls",namespace='pages'))
 ]
