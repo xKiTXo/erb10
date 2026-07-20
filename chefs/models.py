29fd3aa27fa844658d835a70ced7bd1c156f19bf
xKiTXo <cat.ion2@hotmail.com>
2026-07-20 10:14:24 +0800

    update: changes models

--- chefs/models.py
+++ chefs/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Chef(models.Model):
     name=models.CharField(max_length=200)
-    photo=models.ImageField(upload_to="photos/%Y/%m/%d/")
-    description=models.TextField(blank=True)
+    portrait=models.ImageField(upload_to="photos/%Y/%m/%d/")
+    bio=models.TextField(blank=True)
     phone=models.CharField(max_length=20) 
     email=models.CharField(max_length=50) 
     is_mvp=models.BooleanField(default=False)
