25d6253a78f894653b41e906c8ee33f847f94c69
xKiTXo <cat.ion2@hotmail.com>
2026-07-22 10:02:42 +0800

    feat: add listing.html

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Listing
 from django.utils import timezone
 from django.core.paginator import Paginator
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 def listings(request):
     # print(request.path)
-    listings = Listing.objects.filter(is_published=True)
+    listings = Listing.objects.order_by('list_date').filter(is_published=True)
     paginator =  Paginator(listings,3)
     page_number = request.GET.get("page")
     paged_listings =  paginator.get_page(page_number)
@@ -17,8 +17,13 @@ def listings(request):
     }
     return render(request,"pages/listings.html",content)
 
-def listing(request):
-    return render(request,"pages/listing.html",locals())
+def listing(request,listing_id):
+    listing = get_object_or_404(Listing,pk=listing_id)
+    print(listing)
+    context = {
+        "listing":listing
+    }
+    return render(request,"pages/listing.html",context)
 
 def search(request):
     return render(request,"pages/search.html",locals())
