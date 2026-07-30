2b0adf92e8ad549f98dbd1a83402f86e372faa8a
xKiTXo <cat.ion2@hotmail.com>
2026-07-30 09:53:21 +0800

    feat: updated register.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 app_name="accounts"
 urlpatterns=[
-    path("register",views.register,name="register"),
-    path("login",views.login,name="login"),
-    path("logout",views.logout,name="logout"),
-    path("dashboard",views.dashboard,name="dashboard"),
+    path("register/",views.register,name="register"),
+    path("login/",views.login,name="login"),
+    path("logout/",views.logout,name="logout"),
+    path("dashboard/",views.dashboard,name="dashboard"),
 ]
